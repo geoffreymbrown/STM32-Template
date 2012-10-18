1e7c04a492e16aefa0e9ab44a239c53a23b67127
Geoffrey Brown <geoffreymbrown@gmail.com>
2012-10-18 14:53:14 -0400

    added license to ST7735.c

--- Library/ST7735.c
+++ Library/ST7735.c
@@ -1,3 +1,25 @@
+/*************************************************** 
+  This is a library for the Adafruit 1.8" SPI display.
+  This library works with the Adafruit 1.8" TFT Breakout w/SD card  
+  ----> http://www.adafruit.com/products/358  
+  as well as Adafruit raw 1.8" TFT display  
+  ----> http://www.adafruit.com/products/618
+ 
+  Check out the links above for our tutorials and wiring diagrams 
+  These displays use SPI to communicate, 4 or 5 pins are required to  
+  interface (RST is optional) 
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+/* 
+ The code contained here is based upon the adafruit driver, but heavily modified
+*/
+
 #include <stm32f10x.h>
 #include <stm32f10x_rcc.h>
 #include <stm32f10x_gpio.h>
